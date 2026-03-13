0e12c5c76652a403f43b3e7e2e0c458ed651b671
asepewa123 <asepewa123@gmail.com>
2026-03-13 10:14:19 +0100

    Forceer kleine letter voor Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom"
 import {Home} from "./Pages/Home"
 import {CoinDetails} from "./Pages/CoinDetails"
-import { fetchCryptos } from "../Api/coinGecko";
 
 function App() {
 
