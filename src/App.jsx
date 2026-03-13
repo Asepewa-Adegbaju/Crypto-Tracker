2e812ae0a94fbf0d42b38a3ce8aa7b6c79db3827
asepewa123 <asepewa123@gmail.com>
2026-03-13 09:51:17 +0100

    werkend maken voor vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom"
 import {Home} from "./Pages/Home"
 import {CoinDetails} from "./Pages/CoinDetails"
+import { fetchCryptos } from "../Api/coinGecko";
 
 function App() {
 
